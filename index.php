2e288bb90dd6c8e239408160bd7826b39b474d49
Vadim Korostelev <vdmkbu@ya.ru>
2020-02-15 18:52:45 +0500

    тест Geocoder

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: vdmkbu
- * Date: 13.02.20
- * Time: 21:02
- */
---
+++ tests/GecoderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Vdmkbu\Geolocator\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Vdmkbu\Geolocator\Types\Address;
+use Vdmkbu\Geolocator\Types\Point;
+use Vdmkbu\Geolocator\YandexGeocoder;
+
+class GecoderTest extends TestCase
+{
+    /** @test */
+    public function testGeocoder()
+    {
+        $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+
+        $client = null;
+        $api_key = getenv('YANDEX_API_KEY');
+
+        $geocoder = new YandexGeocoder($client, $api_key);
+        $address = $geocoder->geocode(new Point('37.611347,55.760241'));
+        $point = $point = $geocoder->geocode(new Address('Челябинск проспект Ленина 54'));
+
+        self::assertNotNull($address);
+        self::assertNotNull($point);
+
+
+        self::assertEquals("Россия", $address->getCountry());
+        self::assertEquals("6с1", $address->getHouse());
+        self::assertEquals("Тверская улица", $address->getStreet());
+        // and other
+
+        self::assertEquals("55.160818", $point->getLng());
+        self::assertEquals("61.402419", $point->getLat());
+    }
+}
